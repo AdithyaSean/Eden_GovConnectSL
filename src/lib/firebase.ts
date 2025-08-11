5d0fd12c8b48907ac7e192851889b56aa7ee5638
Rivindu Peshara <rivindupeshara11@gmail.com>
2025-08-11 18:06:40 +0000

    documents are not uploading , check from database side even

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,12 @@
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+"use client";
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+import {getStorage} from 'firebase/storage';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  "projectId": "hackthon-e7xuj",
-  "appId": "1:7919745878:web:92f26fb183878dbe3334f7",
-  "storageBucket": "hackthon-e7xuj.appspot.com",
-  "apiKey": "AIzaSyCXdmf4zicHxUsPkcZxkrHiWq8ks-1IqlY",
-  "authDomain": "hackthon-e7xuj.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "7919745878"
-};
+// Your web app's Firebase configuration is automatically provided by Firebase App Hosting.
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG!);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
